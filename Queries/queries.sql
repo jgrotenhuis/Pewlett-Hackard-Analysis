7d00c39a94ea0f502e7a33f5d3564b16036999bf
Jonny Grotenhuis <grotenhuis@google.com>
2020-07-03 12:02:11 -0700

    Challenge Deliverables

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -36,4 +36,136 @@
 --WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 --AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
-COPY retirement_info TO '/Users/grotenhuis/Desktop/Bootcamp/Pewlett-Hackard-Analysis/Data/retirement_info_output.CSV' DELIMITER ',' CSV HEADER;
+--COPY retirement_info TO '/Users/grotenhuis/Desktop/Bootcamp/Pewlett-Hackard-Analysis/Data/retirement_info_output.CSV' DELIMITER ',' CSV HEADER;
+
+--DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+--Select emp_no, first_name, last_name
+--Into retirement_info
+--From employees
+--WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+--AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+--SELECT * FROM retirement_info; 
+
+-- Joining departments and dept_manager tables
+--SELECT d.dept_name,
+--     dm.emp_no,
+--     dm.from_date,
+--     dm.to_date
+--FROM departments as d
+--INNER JOIN dept_manager as dm
+--ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+--select ri.emp_no,
+--	ri.first_name,
+--	ri.last_name,
+--	de.to_date
+
+--into current_emp
+	
+--from retirement_info as ri
+--left join dept_emp as de
+--on ri.emp_no = de.emp_no
+--where de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+--select count(ce.emp_no),
+--			 de.dept_no
+--Into emp_count_by_dep_num
+--from current_emp as ce
+--left join dept_emp as de
+--on ce.emp_no = de.emp_no
+--group by de.dept_no
+--order by de.dept_no;
+
+--SELECT * FROM salaries
+--order by to_date desc;
+
+--select e.emp_no,
+--	   e.first_name,
+--	   e.last_name,
+--	   e.gender,
+--	   s.salary,
+--	   de.to_date
+--INTO emp_info
+--FROM employees as e
+--inner join salaries as s on (e.emp_no = s.emp_no)
+--inner join dept_emp as de on (e.emp_no = de.emp_no)
+--WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+--AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+--and (de.to_date = '9999-01-01');
+
+--COPY emp_info TO '/Users/grotenhuis/Desktop/Bootcamp/Pewlett-Hackard-Analysis/Data/emp_info_output.CSV' DELIMITER ',' CSV HEADER;
+
+-- List of managers per department
+--select dm.dept_no,
+--	   d.dept_name, 
+--	   dm.emp_no,
+--	   ce.last_name,
+--	   ce.first_name,
+--	   dm.from_date,
+--	   dm.to_date
+--into manager_info
+--from dept_manager as dm
+--inner join departments as d on (dm.dept_no = d.dept_no)
+--inner join current_emp as ce on (dm.emp_no = ce.emp_no)
+
+--COPY manager_info TO '/Users/grotenhuis/Desktop/Bootcamp/Pewlett-Hackard-Analysis/Data/manager_info_output.CSV' DELIMITER ',' CSV HEADER;
+
+--select ce.emp_no,
+--	   ce.first_name,
+--	   ce.last_name,
+--	   d.dept_name
+--into dept_info
+--from current_emp as ce
+--inner join dept_emp as de on (ce.emp_no = de.emp_no)
+--inner join departments as d on (de.dept_no = d.dept_no)
+
+-- COPY dept_info TO '/Users/grotenhuis/Desktop/Bootcamp/Pewlett-Hackard-Analysis/Data/dept_info_output.CSV' DELIMITER ',' CSV HEADER;
+
+--select emp_no,
+--	   first_name,
+--	   last_name,
+
+-- COPY retirement_info TO '/Users/grotenhuis/Desktop/Bootcamp/Pewlett-Hackard-Analysis/Data/retirement_info_output.CSV' DELIMITER ',' CSV HEADER;
+
+-- Find the number of people in the Sales and Development departments who are going to retire.
+--select ri.emp_no,
+--	   ri.first_name,
+--	   ri.last_name,
+--	   d.dept_name
+--from retirement_info as ri   
+--left join dept_emp as de on (ri.emp_no = de.emp_no)
+--left join departments as d on (de.dept_no = d.dept_no)
+--where de.dept_no in ('d007','d005') 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Challenge Deliverable 1: Create a table for the number of employees who are about to retire grouped by job title.
+select ri.emp_no,
+ 	   CONCAT(ri.first_name, ' ', ri.last_name),
+	   ti.title,
+	   ti.from_date,
+	   sal.salary
+	   ----------------CONCAT(c.FIRSTNAME, ',', c.LASTNAME)
+--into num_retiring_emps_by_title
+from retirement_info as ri 
+inner join titles as ti on (ri.emp_no = ti.emp_no)
+inner join salaries as sal on (ri.emp_no = sal.emp_no)
+order by ti.title asc;
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Pewlett-Hackard-Analysis
+
+Bobby's manager needs a variety of insights in order to first understand how many all up employees are going to retire soon and then to see that broken down by various attributes, such as title, birth date, etc.  It sounds like a large portion of a company's workforce retiring all at once can have a variety of downsides, from brain/work experience drain to incurred costs depending on whether or not the company has promised pensions and other retirement-IOUs.   Helping Pewlett Hackard prepare for this will be crucial, so understanding the scale and nature of this "silver tsunami" as well as how to keep their senior talent engaged for longer in the company through a mentorship program.  
+
+
+>>> 1st Technical Deliverable <<< 
+
+I identified and created table(s0 for the following:
+
+1) showing number of [titles] retiring
+2) one showing number of employees with each title
+3) one showing a list of current employees born between Jan. 1, 1952 and Dec. 31, 1955
+
+General Query Explanation
+After creating the query and new table for the 1st item above, then I build queries for the following questions based on the new tables created.  Each time I created a new table, I had to use that to answer the following question, so they each built on each other.  In summary, I started this challenge with all the data I needed but they were in different tables and not filtered.
+
+My Findings
+1) After removing dupe profiles, I realized that a pretty large sum of employees have been promoted over the years
+2) After doing a count of total employees who are retiring by title, I found that 41,380 employees are nearing retirement and that a large number of them are nearing retirement with senior job titles like staff.  
+
+
+>>> 2nd Technical Deliverable <<<
+
+I identified and created table(s) for the following:
+1) show the number of employees who are eligible for the mentorship program
+
+General Query Explanation
+I accessed data from three tables: employees, dept_emp, and titles to get the answers that I needed and then used partition (I.e. dupe removal) to get rid of the duplicate profiles.
+
+My findings:
+
+1) 1,940 employees are eligible for the mentorship program.
+
+
+Further Analysis Recommendation
+1) I would suggest looking into the company's pension plans and other IOUs to see how much money the company can save by keeping people still on payroll in a limited capacity like 0.5 FTE that way they can delay pension payouts or other IOUs. This would require doing additional joins from new tables that would include sensitive information and probably some high level approvals for access. 
+
+Extras
+1) I also learned how to use CONCAT to combine column headers like first_name and last_name
+
